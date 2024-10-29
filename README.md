c9408cb11f24cd759fce1b1f7438b256685934e6
naikpriti <priti.naik@lexisnexisrisk.com>
2024-10-29 19:56:35 +0530

    added changes

---
+++ README.md
@@ -0,0 +1,20 @@
+
+```
+test_automation
+├─ automation_scripts
+│  ├─ init.py
+│  └─ version.txt
+└─ key-vault
+   └─ variables.tf
+
+```
+```
+test_automation
+├─ automation_scripts
+│  ├─ init.py
+│  └─ version.txt
+├─ key-vault
+│  └─ variables.tf
+└─ README.md
+
+```
--- automation_scripts/init.py
+++ automation_scripts/init.py
@@ -117,7 +117,7 @@ def update_files(branch_name, new_tag_name, ip_addresses):
     subprocess.run(["terraform", "fmt", variable_tf_path])
 
     # Update version.txt
-    version_txt_path = os.path.join("automation_scripts", "version.txt")
+    version_txt_path =  'version.txt'
     with open(version_txt_path, "w") as f:
         f.write(f"Version: {new_tag_name}")
 
@@ -166,7 +166,7 @@ def update_main_branch(ip_addresses, new_tag_name):
     subprocess.run(["terraform", "fmt", variable_tf_path])
 
     # Update version.txt
-    version_txt_path = os.path.join("automation_scripts", "version.txt")
+    version_txt_path = 'version.txt'
     with open(version_txt_path, "w") as f:
         f.write(f"Version: {new_tag_name}")
 
@@ -247,7 +247,7 @@ def fetch_and_process_json():
     version_formatted = f"{version_date[:4]}.{version_date[4:6]}.{version_date[6:]}"
 
     # Step 4: Read the current version from version.txt
-    version_file = os.path.join("./automation_scripts", "version.txt")
+    version_file = 'version.txt'
     with open(version_file, 'r') as file:
         current_version_line = next((line for line in file if line.startswith('Version:')), None)
         if current_version_line:
