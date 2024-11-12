3b7b60bd88154f52ee8484279446d0c659567881
naikpriti <priti.naik@lexisnexisrisk.com>
2024-11-12 21:22:27 +0530

    add  changes

--- automation_scripts/az_ip_update.sh
+++ automation_scripts/az_ip_update.sh
@@ -9,9 +9,14 @@ ACCOUNT_NAME=$(jq -r '.repositories[0].account_name' "$CONFIG_FILE")
 CURRENT_IPS=$(az cognitiveservices account network-rule list --resource-group "$RESOURCE_GROUP" --name "$ACCOUNT_NAME" --query "ipRules[].value" -o tsv)
 echo "Current IP addresses: $CURRENT_IPS"
 
+# Read IP addresses from the file and print each IP address
+echo "IP addresses from ip_addresses.txt:"
+cat ip_addresses.txt
+
 # Read IP addresses from the file and add only new IP addresses
 while IFS= read -r IP; do
     IP=$(echo "$IP" | tr -d '"' | xargs)
+    echo "Processing IP: $IP"
     if ! echo "$CURRENT_IPS" | grep -q "$IP"; then
         az cognitiveservices account network-rule add --resource-group "$RESOURCE_GROUP" --name "$ACCOUNT_NAME" --ip-address "$IP" > /dev/null 2>&1
         if [ $? -eq 0 ]; then
