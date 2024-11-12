664e5af20795544b6f4fdcd74396b57638e37c2e
naikpriti <priti.naik@lexisnexisrisk.com>
2024-11-12 15:04:49 +0530

    add  changes

--- automation_scripts/az_ip_update.sh
+++ automation_scripts/az_ip_update.sh
@@ -7,13 +7,10 @@ CONFIG_FILE="./automation_scripts/input.json"
 RESOURCE_GROUP=$(jq -r '.repositories[0].resource_group' "$CONFIG_FILE")
 ACCOUNT_NAME=$(jq -r '.repositories[0].account_name' "$CONFIG_FILE")
 
-cat ip_addresses.txt
-
-
 # Read IP addresses from the file
 IP_ADDRESSES=$(cat ip_addresses.txt)
 
 # Loop through each IP address and execute the Azure CLI command
 for IP in $IP_ADDRESSES; do
-    "az cognitiveservices account network-rule add --resource-group \"$RESOURCE_GROUP\" --name \"$ACCOUNT_NAME\" --ip-address \"$IP\""
+    az cognitiveservices account network-rule add --resource-group "$RESOURCE_GROUP" --name "$ACCOUNT_NAME" --ip-address "$IP"
 done
