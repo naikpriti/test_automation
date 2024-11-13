b48049e56c8ad305e03211754301efe6ee551e79
naikpriti <priti.naik@lexisnexisrisk.com>
2024-11-13 06:17:55 +0530

    add changes

--- automation_scripts/az_ip_update.sh
+++ automation_scripts/az_ip_update.sh
@@ -14,17 +14,17 @@ echo "IP addresses from ip_addresses.txt:"
 cat ip_addresses.txt
 
 # Read IP addresses from the file and add only new IP addresses
-while IFS= read -r IP; do
-    IP=$(echo "$IP" | tr -d '"' | xargs)
-    echo "Processing IP: $IP"
-    if ! echo "$CURRENT_IPS" | grep -q "$IP"; then
-        az cognitiveservices account network-rule add --resource-group "$RESOURCE_GROUP" --name "$ACCOUNT_NAME" --ip-address "$IP" > /dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo "Successfully added IP: $IP"
-        else
-            echo "Failed to add IP: $IP"
-        fi
-    else
-        echo "IP already exists: $IP"
-    fi
-done < ip_addresses.txt
+#while IFS= read -r IP; do
+#    IP=$(echo "$IP" | tr -d '"' | xargs)
+#    echo "Processing IP: $IP"
+#    if ! echo "$CURRENT_IPS" | grep -q "$IP"; then
+#        az cognitiveservices account network-rule add --resource-group "$RESOURCE_GROUP" --name "$ACCOUNT_NAME" --ip-address "$IP" > /dev/null 2>&1
+#        if [ $? -eq 0 ]; then
+#            echo "Successfully added IP: $IP"
+#        else
+#            echo "Failed to add IP: $IP"
+#        fi
+#    else
+#        echo "IP already exists: $IP"
+#    fi
+#done < ip_addresses.txt
