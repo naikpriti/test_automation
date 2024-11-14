7e752c5340c21732972ccdd35255a848b76b645a
naikpriti <priti.naik@lexisnexisrisk.com>
2024-11-14 11:33:53 +0530

    add changes

--- automation_scripts/az_ip_update.sh
+++ automation_scripts/az_ip_update.sh
@@ -9,27 +9,24 @@ ACCOUNT_NAME=$(jq -r '.repositories[0].account_name' "$CONFIG_FILE")
 CURRENT_IPS=$(az cognitiveservices account network-rule list --resource-group "$RESOURCE_GROUP" --name "$ACCOUNT_NAME" --query "ipRules[].value" -o tsv)
 echo "Current IP addresses: $CURRENT_IPS"
 
+# Remove all existing IP rules
+for IP in $CURRENT_IPS; do
+    echo "Removing IP: $IP"
+    az cognitiveservices account network-rule remove --resource-group "$RESOURCE_GROUP" --name "$ACCOUNT_NAME" --ip-address "$IP" > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Successfully removed IP: $IP"
+    else
+        echo "Failed to remove IP: $IP"
+    fi
+done
+
 # Read IP addresses from the file and print each IP address
 echo "IP addresses from ip_addresses.txt:"
 cat ip_addresses.txt
 
-# Read IP addresses from the file into a set
-declare -A IP_SET
+# Add new IP addresses
 while IFS= read -r IP; do
     IP=$(echo "$IP" | tr -d '"' | xargs)
-    IP_SET["$IP"]=1
-done < ip_addresses.txt
-
-# Determine IPs to add
-NEW_IPS=()
-for IP in "${!IP_SET[@]}"; do
-    if ! echo "$CURRENT_IPS" | grep -q -w "$IP"; then
-        NEW_IPS+=("$IP")
-    fi
-done
-
-# Add new IP addresses
-for IP in "${NEW_IPS[@]}"; do
     echo "Adding IP: $IP"
     az cognitiveservices account network-rule add --resource-group "$RESOURCE_GROUP" --name "$ACCOUNT_NAME" --ip-address "$IP" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
@@ -37,4 +34,4 @@ for IP in "${NEW_IPS[@]}"; do
     else
         echo "Failed to add IP: $IP"
     fi
-done
+done < ip_addresses.txt
