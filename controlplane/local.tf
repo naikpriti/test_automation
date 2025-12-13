cb2450aa8ba25bf8d7644de4a60f51c2c3ea0f4d
naikpriti <priti.naik@elexisnexisrisk.com>
2025-12-13 04:26:54 +0000

    Update variables.tf, version.txt, and module versions for release v1.4.21

--- controlplane/local.tf
+++ controlplane/local.tf
@@ -16,7 +16,7 @@ locals {
     free     = "Free"
     standard = "Standard"
   }
-  module_version = "v1.4.20"
+  module_version = "v1.4.21"
   maintenance_utc_offset_lookup = {
     westeurope = "+00:00"
     uksouth    = "+00:00"
